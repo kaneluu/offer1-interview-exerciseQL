c18d100b3864296f505c99e4873fc46377c5d14a
Quyen Luu <qluu8268@sdsu.edu>
2021-06-23 14:36:00 -0700

    Testing displaying data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import data from "./homes.json";
 
 function App() {
+  // const [] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-body">
+        Welcome to Offer1!!!
+         {data.map((post, index) => {
+          return(
+            <div key = {post.id}>
+            <p>{post.state} {post.price}</p>
+            <p>{post.listingAgent.id} {post.listingAgent.licenseNumber}</p>
+            </div>
+          )
+        })} 
+        
       </header>
-    </div>
-  );
+   </div>
+    // <div>
+    //   {
+    //     Object.keys(data.object).map((key,i) =>(
+    //       <p key = {i}>
+    //         <span>Key Name: {key}</span>
+    //         <span>Value: {data.object[key]}</span>
+    //       </p>
+    //     ))
+    //   }
+    // </div>
+    // <div className = "App">
+    //   <Houses />
+    // </div>
+  )
 }
 
 export default App;
+// ReactDOM.render(<App />, document.getElementById('app'));
